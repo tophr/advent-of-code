d6c37fbca75eb9fdc5ed8a114053867c5882d392
Topher McCulloch <topher@pivotdesign.com>
2020-12-03 20:03:29 -0600

    Adds part 2 for day 3

--- day03.js
+++ day03.js
@@ -336,21 +336,43 @@ var trees = `.#..........#...#...#..#.......
 // var treeArray = sampleTrees.split("\n");
 var treeArray = trees.split("\n");
 // console.log(treeArray);
-var i = 0;
-var treeCounter = 0;
+
 var rowLength = treeArray[0].length;
 
-treeArray.forEach( (row) => {
+function slopeCheckDeux( trees, right, down ) {
+  var i = 0;
+  var i2 = 0;
+  var treeCount = 0;
+  trees.forEach( (tree) => {
+    var downOffset = i % down;
+    // console.log(downOffset);
+    if ( downOffset === 0 ) {
+      var pointer = ( ( i2 * right ) + 1 ) % rowLength;
+      // console.log(pointer);
 
-  var pointer = ( ( i * 3 ) + 1 ) % rowLength;
-  // console.log(pointer);
+      var pointerIndex = pointer === 0 ? rowLength - 1 : pointer -1;
+      // console.log({pointerIndex});
 
-  var pointerIndex = pointer === 0 ? rowLength - 1 : pointer -1;
-  // console.log({pointerIndex});
+     if ( tree.charAt( pointerIndex ) === '#' ) {
+       treeCount++;
+     };
+     i2++;
+    }
+    i++;
+  });
+  return treeCount;
+}
 
- if ( row.charAt( pointerIndex ) === '#' ) {
-   console.log('tree!!!');
- };
+var slope1 = slopeCheckDeux(treeArray, 1, 1);
+var slope2 = slopeCheckDeux(treeArray, 3, 1);
+var slope3 = slopeCheckDeux(treeArray, 5, 1);
+var slope4 = slopeCheckDeux(treeArray, 7, 1);
+var slope5 = slopeCheckDeux(treeArray, 1, 2);
 
- i++;
-});
+console.log( slope1 );
+console.log( slope2 );
+console.log( slope3 );
+console.log( slope4 );
+console.log( slope5 );
+
+console.log('total is ' + slope1 * slope2 * slope3 * slope4 * slope5 );
