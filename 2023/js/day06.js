a38d0dfd3d081b29738eb6640d1cb384119050ce
Topher McCulloch <tophermcculloch@gmail.com>
2023-12-06 10:33:28 -0600

    Completes day 6 part 2

--- 2023/js/day06.js
+++ 2023/js/day06.js
@@ -6,8 +6,6 @@ const records = ``;
 const recordsArray = records.split("\n");
 let times = recordsArray[0].split(/\s+/).slice(1);
 let distances = recordsArray[1].split(/\s+/).slice(1);
-console.log(times);
-console.log(distances);
 
 let winningPossibilities = [];
 times.forEach((time, i) => {
@@ -25,10 +23,34 @@ times.forEach((time, i) => {
   }
 });
 
-console.log(winningPossibilities);
+// console.log(winningPossibilities);
 function calculateArrayProduct(arrays) {
   return arrays.reduce((product, array) => product * array.length, 1);
 }
 
 const result = calculateArrayProduct(winningPossibilities);
-console.log(result); 
+console.log("Part one solution is " + result); 
+
+// Part Two 
+let kernTime = recordsArray[0].split(":").slice(1)[0].replace(/\s/g, "");
+let kernDistance = recordsArray[1].split(":").slice(1)[0].replace(/\s/g, "");
+// console.log(kernTime);
+// console.log(kernDistance);
+
+function computePartTwo(time, distance) {
+  let raceTime = parseInt(time);
+  let recordDistance = parseInt(distance);
+  let holdTime = 0;
+  let totalPossibilities = 0; 
+  while (holdTime < raceTime) {  
+    let raceDistance = (raceTime - holdTime) * holdTime;
+    // console.log(`Time: ${raceTime} Hold: ${holdTime} Distance: ${raceDistance}`);
+    if ( raceDistance > recordDistance ) {
+      // winningPossibilities2.push(holdTime);
+      totalPossibilities++;
+    }
+    holdTime++;
+  }
+  return totalPossibilities++;
+}
+console.log( "Part two solution is " + computePartTwo(kernTime, kernDistance) );
