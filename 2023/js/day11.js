d0b12ce6c021051c7ff867e51369812485a90679
Topher McCulloch <tophermcculloch@gmail.com>
2023-12-11 19:51:15 -0600

    Completes day 11 part 2

--- 2023/js/day11.js
+++ 2023/js/day11.js
@@ -11,7 +11,7 @@ const galaxyEx = `...#......
 
 const galaxy = ``;
 
-const lines = galaxyEx.split("\n");
+const lines = galaxy.split("\n");
 let galaxyCount = 1;
 let emptyRows = [];
 // Create emptyCols array and fill it with numbers from 0 to lines[0].length - 1
@@ -43,15 +43,18 @@ console.log(lines);
 console.log(emptyRows);
 console.log(emptyCols);
 
-// Expand galaxy doubling the amount of . for each empty row and column
+// Create a deep copy of lines
 let newGalaxy = [];
+lines.forEach((line) => {
+  newGalaxy.push(line.slice());
+});
 emptyRows.reverse();
 emptyCols.reverse();
 // Double the columns
 lines.forEach((line, i) => {
   emptyCols.forEach((col, j) => {
     // Add "." to line at position col
-    lines[i].splice(col, 0, ".");
+    newGalaxy[i].splice(col, 0, ".");
   });
 });
 // Double the rows
@@ -60,20 +63,21 @@ emptyRows.forEach((row, i) => {
   for (let j = 0; j < lines[0].length; j++) {
     newRow.push(".");
   }
-  lines.splice(row, 0, newRow);
+  newGalaxy.splice(row, 0, newRow);
 });
 
-console.log(lines);
+console.log(newGalaxy);
 
 // Map coordinates of each galaxy
 let galaxyMap = [];
-lines.forEach((line, i) => {
+newGalaxy.forEach((line, i) => {
   line.forEach((char, j) => {
     if (char !== ".") {
       galaxyMap.push({x: j, y: i, galaxy: char});
     }
   });
 });
+console.log("galaxy map one");
 console.log(galaxyMap);
 
 function computeDistance(galaxy1, galaxy2) {
@@ -94,4 +98,48 @@ let galaxyDistancesSum = 0;
 galaxyDistances.forEach((distance) => {
   galaxyDistancesSum += parseInt(distance.distance);
 });
-console.log(galaxyDistancesSum);
+console.log("solution for part one is " + galaxyDistancesSum);
+
+// Part Two - this could be much dryer 
+// Map coordinates of orginal galaxy
+let galaxyMapExpanded = [];
+function computeExpandedCoordinates(galaxy, power) {
+  let newGalaxy = {x: galaxy.x, y: galaxy.y, galaxy: galaxy.galaxy};
+  emptyCols.forEach((col) => {
+    if (col < galaxy.x) {
+      newGalaxy.x = newGalaxy.x + ( power * 1) - 1;
+    }
+  });
+  emptyRows.forEach((row) => {
+    if (row < galaxy.y) {
+      newGalaxy.y = newGalaxy.y + ( power * 1 ) - 1;
+    }
+  });
+  return newGalaxy;
+}
+
+lines.forEach((line, i) => {
+  line.forEach((char, j) => {
+    if (char !== ".") {
+      galaxyMapExpanded.push( computeExpandedCoordinates({x: j, y: i, galaxy: char}, 1000000));
+    }
+  });
+});
+console.log("galaxy map two");
+console.log(galaxyMapExpanded);
+
+// Compute distance between each galaxy
+let galaxyDistancesExpanded = [];
+for (let i = 0; i < galaxyMapExpanded.length; i++) {
+  for (let j = i + 1; j < galaxyMapExpanded.length; j++) {
+    galaxyDistancesExpanded.push({galaxy1: galaxyMapExpanded[i].galaxy, galaxy2: galaxyMapExpanded[j].galaxy, distance: computeDistance(galaxyMapExpanded[i], galaxyMapExpanded[j])});
+  }
+}
+console.log(galaxyDistancesExpanded);
+
+// Sum distances for each galaxy
+let galaxyDistancesSum2 = 0;
+galaxyDistancesExpanded.forEach((distance) => {
+  galaxyDistancesSum2 += parseInt(distance.distance);
+});
+console.log("solution for part two is " + galaxyDistancesSum2);
